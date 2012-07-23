ea0c9f3e3b6c03f4ca0d3144ac23a9977c4610b5
Apprentice <apprentice@devbootcamp.com>
2012-07-22 21:43:37 -0700

    trying to commit clueless closet

--- closet.rb
+++ closet.rb
@@ -2,20 +2,21 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
                                         :database => './closet.db'
-
-class Garment < ActiveRecord::Base
-  belongs_to :type
-  scope :clean, where(:soiled => nil)
-
-  def initialize(opts)
-    @name = opts.fetch(:name) { raise ArgumentError }
+                                        
+class Closet
+  
+  def add_garment
   end
+  
+  def remove_garment
+  end
+  
+  def choose_outfit
+  end
+  
+  def laundry
+  end
+  
 end
 
-class Type < ActiveRecord::Base
-  has_many :garments
 
-  def initialize(name)
-    @name = name
-  end
-end
